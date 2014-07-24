2c76aaf588974718eeb15b3d7981f99cd3d105e7
Johnny Freeman <prsjohnny@gmail.com>
2014-07-24 00:06:44 -0400

    enabled debug mode and set timezone

--- public/index.php
+++ public/index.php
@@ -2,7 +2,14 @@
 
 require_once realpath(__DIR__.'/../vendor/autoload.php');
 
+// required by DateTime object
+// should probably get this from
+// configuration file.
+date_default_timezone_set('America/New_York');
+
 $app = new Silex\Application();
+
+$app['debug'] = true;
 
 $app->get('/', function () use ($app) {
   new SuggestedDeposit(new Envelope());
